0a0985835ff9078ecd50f9d168a5462f9a30fa2c
Khaled Hosny <khaledhosny@eglug.org>
2017-02-05 14:06:40 +0200

    Turn into a class and cleanup things a bit

--- src/typesetter.py
+++ src/typesetter.py
@@ -5,70 +5,79 @@ import sys
 import cairo
 import texlib.wrap as texwrap
 
-MARGIN = 10
+class Typesetter:
+    MARGIN = 10
 
-def create_nodes(text, xadvance, debug=False):
-    nodes = texwrap.ObjectList()
-    nodes.debug = debug
+    def __init__(self, text, surface, width, height, debug=False):
+        self.text = text
+        self.width = width
+        self.height = height
+        self.debug = debug
 
-    for ch in text:
-        if ch in " ":
-            nodes.append(texwrap.Glue(xadvance, xadvance / 2, xadvance / 2))
-        elif ch == "\u00A0": # No-Break Space.
-            nodes.append(texwrap.Penalty(0, texwrap.INFINITY))
-            nodes.append(texwrap.Glue(xadvance, xadvance / 2, xadvance / 2))
-        else:
-            nodes.append(texwrap.Box(xadvance, ch))
-    nodes.add_closing_penalty()
+        cr = self.cr = cairo.Context(surface)
+        cr.select_font_face("Monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        cr.set_font_size(20)
 
-    return nodes
+        self.ascent, self.descent, height, self.xadvance, yadvance = cr.font_extents()
 
-def draw_output(cr, nodes, breaks, lengths):
-    ascent, descent, height, xadvance, yadvance = cr.font_extents()
-    cr.set_source_rgb(0, 0, 0)
+    def output(self):
+        self._create_nodes()
+        self._compute_breaks()
+        self._draw_output()
 
-    line_start = 0
-    line = 0
-    for breakpoint in breaks[1:]:
-        y = MARGIN + (ascent + descent) * (line + 1)
-        x = MARGIN
+    def _create_nodes(self):
+        nodes = self.nodes = texwrap.ObjectList()
+        nodes.debug = self.debug
 
-        ratio = nodes.compute_adjustment_ratio(line_start, breakpoint, line, lengths)
-        line += 1
-        for i in range(line_start, breakpoint):
-            box = nodes[i]
-            if box.is_glue():
-                width = box.compute_width(ratio)
-                x += width
-            elif box.is_box():
-                cr.move_to(x, y)
-                cr.show_text(box.character)
-                x += box.width
+        adv = self.xadvance
+
+        for ch in self.text:
+            if ch in " ":
+                nodes.append(texwrap.Glue(adv, adv / 2, adv / 2))
+            elif ch == "\u00A0": # No-Break Space.
+                nodes.append(texwrap.Penalty(0, texwrap.INFINITY))
+                nodes.append(texwrap.Glue(adv, adv / 2, adv / 2))
             else:
-                pass
-        line_start = breakpoint + 1
+                nodes.append(texwrap.Box(adv, ch))
+        nodes.add_closing_penalty()
 
-def main(text, width, debug=False):
-    lengths = [width]
+    def _compute_breaks(self):
+        lengths = [self.width]
+        self.breaks = self.nodes.compute_breakpoints(lengths)
 
-    width = width + 2 * MARGIN
-    height = 500 + 2 * MARGIN
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+    def _draw_output(self):
+        self.cr.set_source_rgb(0, 0, 0)
 
-    cr = cairo.Context(surface)
-    cr.select_font_face("Monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    cr.set_font_size(20)
-    cr.set_source_rgb(1, 1, 1)
-    cr.rectangle(0, 0, width, height)
-    cr.fill()
+        lengths = [self.width]
+        line_start = 0
+        line = 0
+        for breakpoint in self.breaks[1:]:
+            y = self.MARGIN + (self.ascent + self.descent) * (line + 1)
+            x = self.MARGIN
 
-    ascent, descent, height, xadvance, yadvance = cr.font_extents()
+            ratio = self.nodes.compute_adjustment_ratio(line_start, breakpoint, line, lengths)
+            line += 1
+            for i in range(line_start, breakpoint):
+                box = self.nodes[i]
+                if box.is_glue():
+                    width = box.compute_width(ratio)
+                    x += width
+                elif box.is_box():
+                    self.cr.move_to(x, y)
+                    self.cr.show_text(box.character)
+                    x += box.width
+                else:
+                    pass
+            line_start = breakpoint + 1
 
-    nodes = create_nodes(text, xadvance, debug)
-    breaks = nodes.compute_breakpoints(lengths)
-    draw_output(cr, nodes, breaks, lengths)
+def main(text, width, debug, filename):
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 1000, 1000)
 
-    surface.write_to_png("test.png")
+    height = 1000
+    typesetter = Typesetter(text, surface, width, height, debug)
+    typesetter.output()
+
+    surface.write_to_png(filename)
 
 if __name__ == "__main__":
-    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), sys.argv[4])
